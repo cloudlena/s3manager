7ca74a434d2901c04fcad706b3bd5e0dd5996cc5
Tobias Fuhrimann <tobias.fuhrimann@swisscom.com>
2016-12-18 22:54:21 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# S3 Manager
+
+Manage S3 buckets from any provider.
+
+## Environment Variables
+
+* `S3_ACCESS_KEY_ID`
+* `S3_SECRET_ACCESS_KEY`
+* `S3_ENDPOINT`: Optional. In case you are using a different S3 provider than AWS. Defaults to `s3.amazonaws.com`
+* `V2_SIGNING`: Optional. In case your S3 provider still uses V2 Signing, set this to `true`
---
+++ handlers.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"path"
+
+	"strings"
+
+	minio "github.com/minio/minio-go"
+)
+
+// indexHandler handles the main page
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	lp := path.Join("templates", "layout.html")
+	ip := path.Join("templates", "index.html")
+
+	t, err := template.ParseFiles(lp, ip)
+	if err != nil {
+		panic(err)
+	}
+
+	buckets, err := minioClient.ListBuckets()
+	if err != nil {
+		panic(err)
+	}
+
+	err = t.ExecuteTemplate(w, "layout", buckets)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// bucketHandler handles the main page
+func bucketHandler(w http.ResponseWriter, r *http.Request) {
+	bucket := strings.Split(r.URL.Path, "/")[2]
+	var objects []minio.ObjectInfo
+
+	lp := path.Join("templates", "layout.html")
+	bp := path.Join("templates", "bucket.html")
+
+	t, err := template.ParseFiles(lp, bp)
+	if err != nil {
+		panic(err)
+	}
+
+	// Create a done channel to control 'ListObjectsV2' go routine.
+	doneCh := make(chan struct{})
+
+	objectCh := minioClient.ListObjectsV2(bucket, "", false, doneCh)
+	for object := range objectCh {
+		if object.Err != nil {
+			fmt.Println(object.Err)
+			return
+		}
+		objects = append(objects, object)
+	}
+
+	err = t.ExecuteTemplate(w, "layout", objects)
+	if err != nil {
+		panic(err)
+	}
+}
