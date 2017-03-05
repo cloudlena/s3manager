78632e1a7290c01a2911f90827279ca95e1706f8
Tobias Fuhrimann <tobias.fuhrimann@swisscom.com>
2017-03-05 14:42:49 +0100

    Remove route name

--- routes.go
+++ routes.go
@@ -4,7 +4,6 @@ import "net/http"
 
 // Route represents a path of the API
 type Route struct {
-	Name        string
 	Method      string
 	Pattern     string
 	HandlerFunc http.HandlerFunc
@@ -15,49 +14,41 @@ type Routes []Route
 
 var routes = Routes{
 	Route{
-		"Redirect to /buckets",
 		"GET",
 		"/",
 		indexPageHandler,
 	},
 	Route{
-		"Load Buckets Page",
 		"GET",
 		"/buckets",
 		bucketsPageHandler,
 	},
 	Route{
-		"Load Bucket Page",
 		"GET",
 		"/buckets/{bucketName}",
 		bucketPageHandler,
 	},
 	Route{
-		"Create Bucket",
 		"POST",
 		"/api/buckets",
 		createBucketHandler,
 	},
 	Route{
-		"Delete Bucket",
 		"DELETE",
 		"/api/buckets/{bucketName}",
 		deleteBucketHandler,
 	},
 	Route{
-		"Download Object",
 		"GET",
 		"/api/buckets/{bucketName}/objects/{objectName}",
 		getObjectHandler,
 	},
 	Route{
-		"Upload Object",
 		"POST",
 		"/api/buckets/{bucketName}/objects",
 		createObjectHandler,
 	},
 	Route{
-		"Delete Object",
 		"DELETE",
 		"/api/buckets/{bucketName}/objects/{objectName}",
 		deleteObjectHandler,
