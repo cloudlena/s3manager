e39c548bc82ff181691e47b6f9f561c8de9b1163
Tobias Fuhrimann <tobias.fuhrimann@swisscom.com>
2017-03-10 10:54:30 +0100

    Move routes to main function

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/gorilla/mux"
 	minio "github.com/minio/minio-go"
 )
 
@@ -19,7 +20,48 @@ func main() {
 		port = "8080"
 	}
 
-	router := NewRouter()
+	s := &Server{
+		s3: NewMinioClient(),
+	}
+
+	router := mux.NewRouter().StrictSlash(true)
+
+	router.
+		Methods("GET").
+		Path("/").
+		HandlerFunc(Chain(IndexHandler, Logger()))
+	router.
+		Methods("GET").
+		Path("/buckets").
+		HandlerFunc(Chain(s.BucketsPageHandler, Logger()))
+	router.
+		Methods("GET").
+		Path("/buckets/{bucketName}").
+		HandlerFunc(Chain(s.BucketPageHandler, Logger()))
+
+	api := router.PathPrefix("/api").Subrouter()
+
+	buckets := api.PathPrefix("/buckets").Subrouter()
+	buckets.
+		Methods("POST").
+		Path("").
+		HandlerFunc(Chain(s.CreateBucketHandler, Logger()))
+	buckets.
+		Methods("DELETE").
+		Path("/{bucketName}").
+		HandlerFunc(Chain(s.DeleteBucketHandler, Logger()))
+	buckets.
+		Methods("POST").
+		Path("/{bucketName}/objects").
+		HandlerFunc(Chain(s.CreateObjectHandler, Logger()))
+	buckets.
+		Methods("GET").
+		Path("/{bucketName}/objects/{objectName}").
+		HandlerFunc(Chain(s.GetObjectHandler, Logger()))
+	buckets.
+		Methods("DELETE").
+		Path("/{bucketName}/objects/{objectName}").
+		HandlerFunc(Chain(s.DeleteObjectHandler, Logger()))
 
 	log.Fatal(http.ListenAndServe(":"+port, router))
 }
