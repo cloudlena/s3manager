84072b3be3e52b22f478cd714fc2c0a709620f99
Lena Fuhrimann <6780471+cloudlena@users.noreply.github.com>
2017-04-09 22:44:06 +0200

    Add message to deleted response

--- delete-bucket_test.go
+++ delete-bucket_test.go
@@ -20,7 +20,7 @@ func TestDeleteBucketHandler(t *testing.T) {
 		"success": {
 			s3:                   &S3ClientMock{},
 			expectedStatusCode:   http.StatusNoContent,
-			expectedBodyContains: "",
+			expectedBodyContains: http.StatusText(http.StatusNoContent),
 		},
 		"s3 error": {
 			s3: &S3ClientMock{
