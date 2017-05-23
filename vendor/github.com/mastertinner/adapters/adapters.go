07340144ec582a396dee94a4e4ad9922059396c2
Tobias Fuhrimann <tobias.fuhrimann@swisscom.com>
2017-05-23 17:18:28 +0200

    Upgrade dependencies

--- vendor/github.com/mastertinner/adapters/adapters.go
+++ vendor/github.com/mastertinner/adapters/adapters.go
@@ -1,6 +1,10 @@
+// Package adapters is a very lightweight framework for HTTP middleware.
 package adapters
 
 import "net/http"
+
+// Adapter is an HTTP middleware.
+type Adapter func(http.Handler) http.Handler
 
 // Adapt adds adapters to an HTTP handler.
 func Adapt(h http.Handler, adapters ...Adapter) http.Handler {
