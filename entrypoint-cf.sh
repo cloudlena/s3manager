66c823e4498757838bc0796420824b5728795277
Tobias Fuhrimann <tobias.fuhrimann@gmail.com>
2017-08-24 10:25:06 +0200

    Adapt to new ECS service from Swisscom App Cloud

--- entrypoint-cf.sh
+++ entrypoint-cf.sh
@@ -18,6 +18,7 @@ if [ -z "${s3_credentials}" ]; then
   exit 1
 fi
 s3_endpoint="$(echo "${s3_credentials}" | jq -r '.accessHost // ""')"
+s3_endpoint=${s3_endpoint#'https://'}
 s3_access_key_id="$(echo "${s3_credentials}" | jq -r '.accessKey // ""')"
 s3_secret_access_key="$(echo "${s3_credentials}" | jq -r '.sharedSecret // ""')"
 
